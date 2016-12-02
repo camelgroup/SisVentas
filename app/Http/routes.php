788fd834f7fa688eb94061d28396411e42a6c088
alep007 <acabrera477@gmail.com>
2016-12-02 01:20:14 -0400

    route ArticuloController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,4 +16,5 @@ Route::get('/', function () {
 });
 
 Route::resource('almacen/categoria','CategoriaController');
+Route::resource('almacen/articulo','ArticuloController');
 Route::resource('ventas/cliente','ClienteController');
